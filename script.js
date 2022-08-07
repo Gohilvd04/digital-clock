81c25e6f0a3683186c99b8e0a716442fd920fc35
Gohil Vishal <91748441+Gohilvd04@users.noreply.github.com>
2022-08-07 22:20:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minutes");
+const second = document.getElementById("seconds");
+const amPm = document.getElementById("ampm");
+
+
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h > 12){
+        h = h - 12;
+        ampm = "PM";
+    }
+   
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+
+    hour.innerText=h;
+    minute.innerText=m;
+    second.innerText=s;
+    amPm, (innerText=ampm);
+
+    console.log(hour)
+    console.log(minute)
+    console.log(second)
+
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+
+updateClock();
